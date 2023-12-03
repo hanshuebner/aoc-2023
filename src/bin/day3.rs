9fdd41be3513d5d1c93f2919bdcca0d9520c5741
Hans Hübner <hans.huebner@gmail.com>
2023-12-03 08:37:30 +0100

    day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,63 +1,131 @@
 use std::env;
 use std::fs::read_to_string;
+use std::collections::HashSet;
 
-type MotorSchematic = Vec<Vec<char>>;
+#[derive(Debug)]
+struct MotorSchematic {
+    input_data: Vec<Vec<char>>,
+    part_numbers: HashSet<usize>,
+    part_number_index: Vec<Vec<Option<usize>>>,
+}
 
 fn read_motor_schematic(lines: &Vec<String>) -> MotorSchematic
 {
+    // Read the input data into a two-dimensional "array" that has a one character around the
+    // border so that we can safely check adjacency without worrying about the edges.
     let width = lines.iter().next().unwrap().len() + 2;
     let height = lines.len() + 2;
 
-    let mut motor_schematic = vec![vec!['.'; width]; height];
+    let mut input_data = vec![vec!['.'; width]; height];
 
     for (index, line) in lines.iter().enumerate() {
-        let row = &mut motor_schematic[index + 1];
+        let row = &mut input_data[index + 1];
         let chars = line.chars().collect::<Vec<char>>();
         row.splice(1..chars.len() + 1, chars);
     }
-    motor_schematic
+
+    // Find the part numbers, building an index from (expanded) input coordinates to part
+    // numbers and a set of all part numbers.
+    let mut part_number_index = vec![vec![None; width]; height];
+    let mut part_numbers: HashSet<usize> = HashSet::new();
+
+    for (row, row_data) in input_data.iter().enumerate() {
+        let mut number_start_col = 0;
+        let mut current_number: usize = 0;
+        let mut in_number = false;
+        let mut is_part_number = false;
+        for (col, c) in row_data.iter().enumerate() {
+            if c.is_digit(10) {
+                if !in_number {
+                    in_number = true;
+                    number_start_col = col;
+                }
+                current_number = current_number * 10 + (*c as usize - '0' as usize);
+                is_part_number = is_part_number || has_adjacent(&input_data, row, col, is_symbol)
+            } else {
+                if in_number {
+                    if is_part_number {
+                        part_numbers.insert(current_number);
+                        for index_col in number_start_col..col {
+                            part_number_index[row][index_col] = Some(current_number);
+                        }
+                    }
+                    current_number = 0;
+                    in_number = false;
+                    is_part_number = false;
+                }
+            }
+        }
+    }
+
+    MotorSchematic {
+        input_data,
+        part_number_index,
+        part_numbers,
+    }
 }
 
 fn is_symbol(c: char) -> bool {
     !c.is_digit(10) && c != '.'
 }
 
-fn has_adjacent_symbol(schematic: &MotorSchematic, row: usize, col: usize) -> bool {
-    assert!(schematic[row][col].is_digit(10));
-    row < schematic.len() - 1
-        && col < schematic[0].len() - 1
-        && (is_symbol(schematic[row - 1][col])
-            || is_symbol(schematic[row - 1][col - 1])
-            || is_symbol(schematic[row][col - 1])
-            || is_symbol(schematic[row + 1][col - 1])
-            || is_symbol(schematic[row + 1][col])
-            || is_symbol(schematic[row + 1][col + 1])
-            || is_symbol(schematic[row][col + 1])
-            || is_symbol(schematic[row - 1][col + 1]))
+fn has_adjacent<F>(
+    input_data: &Vec<Vec<char>>,
+    row: usize,
+    col: usize,
+    predicate: F,
+) -> bool
+    where
+        F: Fn(char) -> bool,
+{
+    assert!(input_data[row][col].is_digit(10));
+    row < input_data.len() - 1
+        && col < input_data[0].len() - 1
+        && (predicate(input_data[row - 1][col])
+        || predicate(input_data[row - 1][col - 1])
+        || predicate(input_data[row][col - 1])
+        || predicate(input_data[row + 1][col - 1])
+        || predicate(input_data[row + 1][col])
+        || predicate(input_data[row + 1][col + 1])
+        || predicate(input_data[row][col + 1])
+        || predicate(input_data[row - 1][col + 1]))
 }
 
-fn find_and_sum_part_numbers(lines: &Vec<String>) -> usize
-{
-    let schematic = read_motor_schematic(lines);
-    let mut sum: usize = 0;
+fn adjacent_part_numbers(schematic: &MotorSchematic, row: usize, col: usize) -> HashSet<usize> {
+    let mut result: HashSet<usize> = HashSet::new();
+    let index = &schematic.part_number_index;
+    for candidate in [
+        index[row - 1][col],
+        index[row - 1][col - 1],
+        index[row][col - 1],
+        index[row + 1][col - 1],
+        index[row + 1][col],
+        index[row + 1][col + 1],
+        index[row][col + 1],
+        index[row - 1][col + 1],
+    ] {
+        if let Some(part_number) = candidate {
+            result.insert(part_number);
+        }
+    }
+    return result;
+}
 
-    for (row, row_data) in schematic.iter().enumerate() {
-        let mut current_number: usize = 0;
-        let mut in_number = false;
-        let mut is_part_number = false;
+fn find_and_sum_part_numbers(schematic: &MotorSchematic) -> usize
+{
+    schematic.part_numbers.iter().sum()
+}
+
+fn find_and_sum_gear_ratios(schematic: &MotorSchematic) -> usize
+{
+    let mut sum: usize = 0;
+    for (row, row_data) in schematic.input_data.iter().enumerate() {
         for (col, c) in row_data.iter().enumerate() {
-            if c.is_digit(10) {
-                in_number = true;
-                current_number = current_number * 10 + (*c as usize - '0' as usize);
-                is_part_number = is_part_number || has_adjacent_symbol(&schematic, row, col)
-            } else {
-                if in_number {
-                    if is_part_number {
-                        sum = sum + current_number
-                    }
-                    current_number = 0;
-                    in_number = false;
-                    is_part_number = false;
+            if *c == '*' {
+                let part_numbers = adjacent_part_numbers(&schematic, row, col);
+                if part_numbers.len() == 2 {
+                    let v : Vec<&usize> = part_numbers.iter().collect();
+                    sum = sum + v[0] * v[1]
                 }
             }
         }
@@ -68,8 +136,11 @@ fn find_and_sum_part_numbers(lines: &Vec<String>) -> usize
 fn main() {
     let filename = env::args().nth(1).unwrap();
     let lines = read_to_string(filename).unwrap().lines().map(String::from).collect();
-    let sum = find_and_sum_part_numbers(&lines);
-    println!("sum: {sum}");
+    let schematic = read_motor_schematic(&lines);
+    let sum_1 = find_and_sum_part_numbers(&schematic);
+    println!("part 1 sum: {sum_1}");
+    let sum_2 = find_and_sum_gear_ratios(&schematic);
+    println!("part 2 sum: {sum_2}")
 }
 
 #[cfg(test)]
@@ -84,8 +155,9 @@ mod tests {
     fn test_part_1() {
         let lines = TEST_INPUT.lines().map(String::from).collect();
         let schematic = read_motor_schematic(&lines);
-        assert_eq!(schematic[0].len(), TEST_INPUT_WIDTH + 2);
-        assert_eq!(schematic.len(), TEST_INPUT_HEIGHT + 2);
-        assert_eq!(find_and_sum_part_numbers(&lines), 4361);
+        assert_eq!(schematic.input_data[0].len(), TEST_INPUT_WIDTH + 2);
+        assert_eq!(schematic.input_data.len(), TEST_INPUT_HEIGHT + 2);
+        assert_eq!(find_and_sum_part_numbers(&schematic), 4361);
+        assert_eq!(find_and_sum_gear_ratios(&schematic), 467835)
     }
 }
